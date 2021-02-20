d70a2b17f93fe6b14e7cf6ea9dd53dd77d0d73eb
dangwin <danghwin@gmail.com>
2021-02-20 14:50:38 -0600

    commit tree eeb09a07dbb6da42750b80b04e71c094fb672676

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+var element = document.getElementById('main');
+element.remove();
+
+let newHeader = document.createElement('h1');
+
+let attribute = document.createAttribute("id");
+attribute.value = "victory";
+newHeader.setAttributeNode(attribute);
+
+document.body.appendChild(newHeader)
+newHeader.innerHTML = "Dang is the champion";
